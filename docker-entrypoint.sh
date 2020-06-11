ccb1ab71336ba2b2a70341a9f01761b7dd944dea
mark <mark@dumay.nl>
2020-06-11 07:51:11 +0200

    Added discovery of latest casper theme

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+DOWNLOAD_GITHUB=https://github.com/TryGhost/Casper
+GITHUB_RELEASES=/TryGhost/Casper/releases/tag
+DEFAULT_CASPER_VERSION=3.0.12
+
 # Set default values for flags and variables
-CASPER_VERSION=3.0.12
 if [ -z "$THEMES" ]; then THEMES='false'; fi
 
 # Convert selected environment variables to lower case
@@ -11,11 +14,23 @@ THEMES=$(echo "$THEMES" | tr -s '[:upper:]' '[:lower:]')
 mkdir -p "$GHOST_CONTENT"/logs/ && chown node:node "$GHOST_CONTENT"/logs/
 
 # Pull default casper theme if requested
-# TODO: identify latest casper version
 if [ "$THEMES" == 'true' ] ; then
-    echo "Downloading default theme (Casper v$CASPER_VERSION)"
+    echo "Downloading default theme (Casper)"
+    
+    # Identify latest release of Casper, revert to default value if not found
+    CASPER_TAGS=$(curl -s "$DOWNLOAD_GITHUB/tags" | egrep "a href=\"$GITHUB_RELEASES/[0-9]+.[0-9]+.[0-9]+\"")
+    LATEST_CASPER_VERSION=$(echo "$CASPER_TAGS" | head -1 | cut -c 46- | sed "s/\">//g")
+    if [ ! -z "$LATEST_CASPER_VERSION" ] ; then
+        TARGET_CASPER_VERSION="$LATEST_CASPER_VERSION"
+    else
+        echo "Could not identify latest Casper version, reverting to default value"
+        TARGET_CASPER_VERSION="$DEFAULT_CASPER_VERSION"
+    fi
+
+    # Download and extract latest Casper theme
+    echo "Targeted Casper theme: v$TARGET_CASPER_VERSION"
     mkdir -p "$GHOST_CONTENT"/themes/casper/ && chown node:node "$GHOST_CONTENT"/themes/casper/
-    curl -sSL "https://github.com/TryGhost/Casper/archive/$CASPER_VERSION.tar.gz" \
+    curl -sSL "https://github.com/TryGhost/Casper/archive/$TARGET_CASPER_VERSION.tar.gz" \
         | tar -C "$GHOST_CONTENT/themes/casper/" -xz --strip 1
 else
     echo "Skipping downloading of default theme, set THEMES=true to override"
