468a40f6a6936dbbd6e17eece332c98b8997c6d9
mark <mark@dumay.nl>
2021-02-14 10:40:19 +0100

    Embedded nginx templates into image

--- config/ghost/docker/docker_entrypoint.sh
+++ config/ghost/docker/docker_entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 #======================================================================================================================
-# Title         : docker-entrypoint-override.sh
-# Description   : Initializes Ghost with default Casper theme
+# Title         : docker_entrypoint.sh
+# Description   : Runs Ghost using nginx as proxy
 # Author        : Mark Dumay
-# Date          : February 13th, 2020
+# Date          : February 14th, 2020
 # Version       : 1.2.0
-# Usage         : ENTRYPOINT ["docker-entrypoint-override.sh"]
+# Usage         : ENTRYPOINT ["docker_entrypoint.sh"]
 # Repository    : https://github.com/markdumay/ghost-backend.git
 #======================================================================================================================
 
@@ -17,6 +17,10 @@ readonly RED='\e[31m' # Red color
 readonly NC='\e[m'    # No color / reset
 readonly BOLD='\e[1m' # Bold font
 readonly NODE_CMD="node 'current/index.js'"
+readonly TEMPLATE_SOURCE_DIR='/var/lib/nginx/templates'
+readonly SNIPPETS_SOURCE_DIR='/var/lib/nginx/snippets'
+readonly TEMPLATE_TARGET_DIR='/etc/nginx/templates'
+readonly SNIPPETS_TARGET_DIR='/etc/nginx/templates/snippets'
 readonly CONTENT_FOLDERS='/apps /data /images /logs /settings /themes'
 readonly INSTALL_DIR='/var/lib/ghost'
 readonly CONTENT_BASE_DIR='/var/lib/ghost/content'
@@ -58,45 +62,18 @@ print_status () {
     echo "[$(date -u '+%Y-%m-%d %T')] $1"
 }
 
-
 #======================================================================================================================
-# Workflow Functions
+# Validates if a specified fully qualified domain name or subdomain adheres to the expected format.
 #======================================================================================================================
-
+# Arguments:
+#   $1 - Domain to be verified. International names need to be converted to punycode ('xn--*') first.
+# Returns:
+#   Returns 0 if domain is supported, non-zero otherwise.
 #======================================================================================================================
-# Downloads and installs the latest available Casper theme.
-#======================================================================================================================
-# Outputs:
-#   Files written to '/var/lib/ghost/content/themes/casper/'.
-#======================================================================================================================
-execute_download_install_casper() {
-    local version
-    local check
-
-    if [ "${THEMES}" = 'true' ] ; then
-        print_status "INFO Downloading default theme (Casper)"
-
-        # Detect latest available stable Casper version
-        version=$(curl -s "$GITHUB_API" | grep "tag_name" | grep -Eo "[0-9]+.[0-9]+.[0-9]+")
-        if [ -z "${version}" ] ; then
-            print_status "WARNING Could not detect Casper versions available for download, setting default value"
-            version="${DEFAULT_CASPER_VERSION}"
-        fi
-
-        # Download and extract latest Casper theme
-        mkdir -p "${GHOST_CONTENT}"/themes/casper
-        curl -sSL "${DOWNLOAD_GITHUB}/${version}.tar.gz" | tar -C "${INSTALL_DIR}/" -xz --strip 1
-        check=$(cat "${INSTALL_DIR}"/package.json 2> /dev/null | grep "\"version\"" | grep -Eo "[0-9]+.[0-9]+.[0-9]+")
-        
-        # Validate intallation
-        if [ "${check}" = "${version}" ] ; then
-            print_status "INFO Installed Casper theme: v${version}"
-        else
-            print_status "ERROR Could not install Casper theme: v${version}"
-        fi
-    else
-        print_status "INFO Skipping downloading of default theme, set THEMES=true to override"
-    fi
+is_valid_domain() {
+    domain_regex='^((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}$'
+    match=$(echo "$1" | grep -Pi "${domain_regex}")
+    ([ -z "${match}" ] && return 1) || return 0
 }
 
 
@@ -119,14 +96,14 @@ cleanup() {
     # Remove the previously deployed templates
     # TODO: fix
     print_status "INFO Removing nginx configuration templates"
-    templates=$(find /var/lib/nginx/templates/*.template -maxdepth 1 -print0 | xargs -0 -n1 basename)
-    templates=$(echo "${templates}" | sed -e "s|^|/etc/nginx/templates/|")
-    snippets=$(find /var/lib/nginx/snippets/*.conf -maxdepth 1 -print0 | xargs -0 -n1 basename)
-    snippets=$(echo "${snippets}" | sed -e "s|^|/etc/nginx/templates/snippets/|")
+    templates="${DOMAINS_BLOG}".conf.template
+    snippets=$(find "${SNIPPETS_SOURCE_DIR}"/*.conf -maxdepth 1 -print0 | xargs -0 -n1 basename)
+    snippets=$(echo "${snippets}" | sed -e "s|^|${TEMPLATE_SNIPPETS_DIR}/|")
     templates=$(printf "%s\n%s" "${templates}" "${snippets}")
     echo "${templates}"| while read -r file
     do
         rm "${file}"
+        print_status "INFO Removed deployed template: ${file}"
     done
 
     # Stop the main ghost process gracefully
@@ -151,19 +128,36 @@ sig_cleanup() {
 }
 
 #======================================================================================================================
-# Entrypoint for the script. It downloads the default Casper theme if applicable and deploys the provided nginx 
-# templates. The deployed templates are removed when the Docker container is stopped. To this end, an event handler is
-# attached to the INT, QUIT, and TERM signals. The ghost process runs in the background, while an endless loop listens
-# for any signals. When a signal is received, the 'cleanup' function is invoked, which removes the templates and
-# gracefully stops the ghost background process using its PID.
+# Entrypoint for the script. It deploys the provided nginx templates. The deployed templates are removed when the
+# Docker container is stopped. To this end, an event handler is attached to the INT, QUIT, and TERM signals. The ghost 
+# process runs in the background, while an infinite loop listens for any signals. When a signal is received, the 
+# 'cleanup' function is invoked, which removes the templates and gracefully stops the ghost background process using
+# its PID.
 #======================================================================================================================
 main() {
-    # Initialize values for flags and variables
-    [ -z "${THEMES}" ] && THEMES='false'
-    THEMES=$(echo "${THEMES}" | tr -s '[:upper:]' '[:lower:]')
+    # Validate env variables
+    [ -z "${DOMAINS_BLOG}" ] && terminate "Please specify 'DOMAINS_BLOG' environment variable"
+    [ -z "${CACHING}" ] && terminate "Please specify 'CACHING' environment variable"
+    is_valid_domain "${DOMAINS_BLOG}" || terminate "Invalid domain: ${DOMAINS_BLOG}"
+    [ "${CACHING}" != 'cached' ] && [ "${CACHING}" != 'uncached' ] && terminate "Invalid caching value: ${CACHING}"
 
-    # Download default theme if applicable
-    execute_download_install_casper
+    # Copy nginx templates and snippets
+    # TODO: test
+    print_status "INFO Deploying nginx configuration templates (${CACHING})"
+    mkdir -p "${SNIPPETS_TARGET_DIR}"
+    cp "${TEMPLATE_SOURCE_DIR}/ghost-${CACHING}".conf.template "${TEMPLATE_TARGET_DIR}/${DOMAINS_BLOG}".conf.template
+    cp "${SNIPPETS_SOURCE_DIR}"/*.conf "${SNIPPETS_TARGET_DIR}"/
+
+    # Display deployed files
+    # TODO: test
+    templates="${TEMPLATE_TARGET_DIR}/${DOMAINS_BLOG}".conf.template
+    snippets=$(find "${SNIPPETS_TARGET_DIR}"/*.conf -maxdepth 1 -print0 | xargs -0 -n1 basename)
+    snippets=$(echo "${snippets}" | sed -e "s|^|${SNIPPETS_TARGET_DIR}/|")
+    files=$(printf "%s\n%s" "${templates}" "${snippets}")
+    echo "${files}" | while read -r file
+    do
+        print_status "INFO Deployed template: ${file}"
+    done
 
     # Initialize content volume, including default theme (Casper)
     folders=$(echo "${CONTENT_FOLDERS}" | sed "s|/|${CONTENT_BASE_DIR}/|g")
@@ -175,7 +169,7 @@ main() {
     trap 'kill ${!}; sig_cleanup' INT QUIT TERM
 
     # Wait for dependencies and then run Ghost in background; store PID for future use
-    wait && exec /usr/local/bin/docker-entrypoint.sh "$@" & pid="$!"
+    wait && eval "${NODE_CMD}" & pid="$!"
 
     # Listen for any signals indefinitely
     while true
@@ -184,6 +178,4 @@ main() {
     done    
 }
 
-# TODO: temp
-# main "$@"
-echo "Testing..."
+main "$@"
