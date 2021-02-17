7786690b3fc5d800aa9f38f0525fd50ffbad1b3f
mark <mark@dumay.nl>
2021-02-17 09:32:41 +0100

    Updated code layout

--- config/mariadb/init/create_backup_user.sh
+++ config/mariadb/init/create_backup_user.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
 
-#======================================================================================================================
+#=======================================================================================================================
 # Title         : create_backup_user.sh
-# Description   : Creates a backup user for a maria database server
+# Description   : Creates a backup user for a mariadb server using Docker secrets
 # Author        : Mark Dumay
-# Date          : June 17th, 2020
-# Version       : 1.0.0
+# Date          : February 16th, 2021
+# Version       : 1.0.1
 # Usage         : create_backup_user.sh
 # Repository    : https://github.com/markdumay/ghost-backend.git
 # Comments      : Expects Docker secrets 'db_backup_user', 'db_backup_password', and 'db_root_password'
-#======================================================================================================================
+#=======================================================================================================================
 
-#======================================================================================================================
+#=======================================================================================================================
 # Constants
-#======================================================================================================================
+#=======================================================================================================================
 
-DATABASE="$MYSQL_DATABASE"
-USER=$(cat /run/secrets/db_backup_user)
-PASSWORD=$(cat /run/secrets/db_backup_password)
-ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+readonly DATABASE="$MYSQL_DATABASE"
+readonly USER=$(cat /run/secrets/db_backup_user)
+readonly PASSWORD=$(cat /run/secrets/db_backup_password)
+readonly ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
 
-#======================================================================================================================
+#=======================================================================================================================
 # Helper Functions
-#======================================================================================================================
+#=======================================================================================================================
 
-# Prints current progress to the console
+#=======================================================================================================================
+# Print current progress to the console with a timestamp as prefix.
+#=======================================================================================================================
+# Arguments:
+#   $1 - Progress message to display.
+# Outputs:
+#   Writes message to stdout.
+#=======================================================================================================================
 print_status() {
     echo "$(date -u '+%Y-%m-%d %T') 0 $1"
 }
 
-# Display error message and terminate with non-zero error
+#=======================================================================================================================
+# Displays error message on console and terminates with non-zero error.
+#=======================================================================================================================
+# Arguments:
+#   $1 - Error message to display.
+# Outputs:
+#   Writes error message to stderr, non-zero exit code.
+#=======================================================================================================================
 terminate() {
     print_status "[Error] $1"
     echo
@@ -38,15 +52,24 @@ terminate() {
 }
 
 
-#======================================================================================================================
+#=======================================================================================================================
 # Main Script
-#======================================================================================================================
+#=======================================================================================================================
 
-if [ -z "$DATABASE" ] || [ -z "$USER" ] || [ -z "$PASSWORD" ] || [ -z "$ROOT_PASSWORD" ]; then
-    terminate "Database credentials not available, cannot create backup user"
-fi
+#=======================================================================================================================
+# Entrypoint for the script.
+#=======================================================================================================================
+main() {
+    if [ -z "${DATABASE}" ] || [ -z "${USER}" ] || [ -z "${PASSWORD}" ] || [ -z "${ROOT_PASSWORD}" ]; then
+        terminate "Database credentials not available, cannot create backup user"
+    fi
 
-print_status "[Note] Creating mariadb backup user '$USER' for database"
-mysql -uroot -p"$ROOT_PASSWORD" -e "CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASSWORD';"
-mysql -uroot -p"$ROOT_PASSWORD" -e "GRANT RELOAD, PROCESS, LOCK TABLES, REPLICATION CLIENT ON *.* TO '$USER'@'localhost';"
-mysql -uroot -p"$ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+    # TODO: improve script to add backup user
+    print_status "[Note] Creating mariadb backup user '${USER}' for database"
+    mysql -uroot -p"${ROOT_PASSWORD}" -e "CREATE USER '${USER}'@'localhost' IDENTIFIED BY '${PASSWORD}';"
+    mysql -uroot -p"${ROOT_PASSWORD}" -e \
+        "GRANT RELOAD, PROCESS, LOCK TABLES, REPLICATION CLIENT ON *.* TO '${USER}'@'localhost';"
+    mysql -uroot -p"${ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+}
+
+main "$@"
